f9205a234e0736d1ce1191aebc82c9d3bfd49066
Gopibannu <gopichandupaladugu1@gmail.com>
2026-09-03 10:10:58 +0530

    Add new coding file to programming folder

---
+++ Programming_absolute_zero/data_types.py
@@ -0,0 +1,11 @@
+#Write a script that takes mixed input and prints each value's type and size.
+import sys as so #Sys module is used to get size od 
+a = 10
+b = 10.0
+c = True
+print(type(a))
+print(so.getsizeof(a))
+print(type(b))
+print(so.getsizeof(b))
+print(type(c))
+print(so.getsizeof(c))
